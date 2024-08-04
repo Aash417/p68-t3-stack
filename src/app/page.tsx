f24b052907d5bc3b2d630b482a9d9ef88aa3eca5
aashish_k <aashishkathat00@gmail.com>
2024-08-04 22:45:59 +0530

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { db } from "~/server/db";
 
 /* eslint-disable @next/next/no-img-element */
+
+export const dynamic = "force-dynamic";
+
 const mockUrl = [
   "https://utfs.io/f/f20dc5e1-91d9-449a-89a7-a554354fbe81-3c8pri.jpg",
   "https://utfs.io/f/008784a1-02d4-4524-b26e-b53430488a46-jms50v.jpg",
@@ -13,7 +16,7 @@ const mockImgs = mockUrl.map((url, index) => ({
 
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
-  console.log("posts :", posts);
+  // console.log("posts :", posts);
 
   return (
     <main>

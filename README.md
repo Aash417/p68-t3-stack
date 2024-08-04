30da6eb320edd5e7df2dc9baf1fe57e9708895e5
aashish_k <aashishkathat00@gmail.com>
2024-08-04 23:13:55 +0530

    linking the db to the ui

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [x] Make it deploy
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [] Set up a database (vercel postgres)
-- [] Attach db to UI
+- [x] Set up a database (vercel postgres)
+- [x] Attach db to UI
 - [] Add auth (cleark)
 - [] Add image upload
 - [] Error management (senetry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,19 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrl = [
-  "https://utfs.io/f/f20dc5e1-91d9-449a-89a7-a554354fbe81-3c8pri.jpg",
-  "https://utfs.io/f/008784a1-02d4-4524-b26e-b53430488a46-jms50v.jpg",
-];
-
-const mockImgs = mockUrl.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const image = await db.query.image.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   // console.log("posts :", posts);
 
   return (
     <main>
       <div className="flex flex-wrap">
-        {[...mockImgs, ...mockImgs, ...mockImgs].map((image, index) => (
-          <div className="w-48 p-4" key={index}>
+        {image.map((image, index) => (
+          <div className="flex w-48 flex-col p-4" key={index}>
             <img src={image.url} alt="" />
+            <div className="">{image.name}</div>
           </div>
         ))}
       </div>
